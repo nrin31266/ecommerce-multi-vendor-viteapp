22ee8d1ed0d71e81ed0d19104980879e6f8ab615
nrin31266 <162881731+nrin31266@users.noreply.github.com>
2025-02-13 02:48:22 +0700

    start vite app

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import { Button } from '@mui/material'
+import './App.css'
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+function App() {
+  
+
+  return (
+    <div>
+      <h1 className='font-bold text-3xl text-amber-500'>Code with Rin</h1>
+      <Button variant='contained'>Code now</Button>
+      <AddShoppingCartIcon />
+    </div>
+  )
+}
+
+export default App
