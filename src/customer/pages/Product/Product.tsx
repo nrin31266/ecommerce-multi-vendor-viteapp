5cf76186db95de1c92fc803415fb08dc44aae4c1
nrin31266 <162881731+nrin31266@users.noreply.github.com>
2025-02-16 16:53:32 +0700

    setup product page

--- src/customer/pages/Product/Product.tsx
+++ src/customer/pages/Product/Product.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useState } from "react";
+import classes from "./Product.module.css";
+import FilterSection from "./components/FilterSection/FilterSection";
+import ProductCard from "./components/ProductCard/ProductCard";
+import {
+  Box,
+  Divider,
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { FilterAlt } from "@mui/icons-material";
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+  const them = useTheme();
+  const isLarge = useMediaQuery(them.breakpoints.up("lg"));
+  const [sortType, setSortType] = useState();
 
-export default Product
+  const handleChangeSort = () => {};
+
+  return (
+    <div className={`mt-10 -z-10 ${classes.root}`}>
+      <h1 className="text-slate-700 text-3xl text-center font-bold pb-5 px-9 uppercase space-x-2">
+        Women sarees
+      </h1>
+      <div className="lg:flex">
+        <section className={`${classes.filterSection} hidden lg:block w-[20%]`}>
+          <FilterSection />
+        </section>
+        <div
+          className={`${classes.ProductSection} w-full lg:w-[80%] space-y-5 relative`}
+        >
+          <div className="h-[30p] flex justify-between items-center px-9">
+            <div>
+              {!isLarge && (
+                <IconButton>
+                  <FilterAlt />
+                </IconButton>
+              )}
+              {!isLarge && (
+                <Box>
+                  <FilterSection />
+                </Box>
+              )}
+            </div>
+
+            <FormControl sx={{ width: "200px" }} size="small">
+              <InputLabel id="demo-simple-select-label">Sort</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={sortType}
+                label="Sort"
+                onChange={handleChangeSort}
+              >
+                <MenuItem value={"price-low"}>Price: Low - High</MenuItem>
+                <MenuItem value={"price-high"}>Price: Hight - Low</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <Divider />
+          <section>
+            <ProductCard />
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
