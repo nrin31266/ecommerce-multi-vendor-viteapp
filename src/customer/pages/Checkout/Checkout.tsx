695fd2916b9ada876d3e2e4f4bfb98cb364584f8
nrin31266 <162881731+nrin31266@users.noreply.github.com>
2025-02-23 10:47:26 +0700

    checkout

--- src/customer/pages/Checkout/Checkout.tsx
+++ src/customer/pages/Checkout/Checkout.tsx
@@ -1,30 +1,74 @@
-import { Box, Button, IconButton, Modal } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  IconButton,
+  Modal,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 import React, { useState } from "react";
 import AddressCard from "./components/AddressCard/AddressCard";
 import AddressForm from "./components/AddressForm/AddressForm";
 import { Close } from "@mui/icons-material";
+import PricingCart from "../Cart/components/PricingCard/PricingCart";
+import './Checkout.css';
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
   boxShadow: 24,
   p: 4,
-  borderRadius: '4px'
+  width: 500,
+  borderRadius: "4px",
 };
+
+const paymentGateways = [
+  {
+    value: "RAZORPAY",
+    image:
+      "https://cdn.iconscout.com/icon/free/png-512/free-razorpay-logo-icon-download-in-svg-png-gif-file-formats--payment-gateway-brand-logos-icons-1399875.png?f=webp&w=256",
+    label: "Razorpay",
+  },
+  {
+    value: "STRIPE",
+    image:
+      "https://logos-world.net/wp-content/uploads/2021/03/Stripe-Symbol.png",
+    label: "Stripe",
+  },
+  {
+    value: "PAYPAL",
+    image:
+      "https://static.vecteezy.com/system/resources/previews/009/469/637/original/paypal-payment-icon-editorial-logo-free-vector.jpg",
+    label: "Paypal",
+  },
+  {
+    value: "VNPAY",
+    image: "https://assets.topdev.vn/images/2020/08/25/VNPAY-Logo-yGapP.png",
+    label: "VNPay",
+  },
+];
 
 const Checkout = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
-  const handleClose = ()=>{
+  const handleClose = () => {
     setIsOpenModal(false);
+  };
+
+  const handleOpenModal = () => {
+    setIsOpenModal(true);
+  };
+
+  const handlePaymentChange = (event: React.ChangeEvent<HTMLInputElement>)=>{
+    setPaymentGateway(event.target.value);
   }
 
-  const handleOpenModal = ()=>{
-    setIsOpenModal(true);
-  }
+  const [paymentGateway, setPaymentGateway] = useState(paymentGateways[0].value);
 
   return (
     <>
@@ -33,7 +77,9 @@ const Checkout = () => {
           <div className="col-span-2 space-y-5">
             <div className="flex justify-between items-center">
               <h1 className="font-bold">Select Delivery Address</h1>
-              <Button onClick={handleOpenModal} variant="outlined">Add New Address</Button>
+              <Button onClick={handleOpenModal} variant="outlined">
+                Add New Address
+              </Button>
             </div>
 
             <div className="text-xs font-medium space-y-5">
@@ -48,6 +94,43 @@ const Checkout = () => {
               </div>
             </div>
           </div>
+          <div className="space-y-5">
+            <div className="border rounded-md border-gray-200 p-5 space-y-5">
+              <div>
+                <h1 className="text-center text-lg text-[var(--primary-color)]">
+                  Choose Payment Gateway
+                </h1>
+              </div>
+              <FormControl className="w-full">
+                <RadioGroup
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  defaultValue="female"
+                  name="radio-buttons-group"
+                  onChange={handlePaymentChange}
+                  value={paymentGateway}
+                >
+                  <div className="flex flex-wrap gap-3 items-center">
+                  {paymentGateways.map((item) => (
+                    <FormControlLabel
+                      className="border rounded-md border-gray-200 w-[120px] h-[60px] p-2"
+                      value={item.value}
+                      control={<Radio />}
+                      label={<img src={item.image} alt="" />}
+                    />
+                  ))}
+                  </div>
+                </RadioGroup>
+              </FormControl>
+            </div>
+            <div className="border border-gray-200 rounded-md">
+              <PricingCart />
+              <div className="px-5 pb-5">
+                <Button size="large" variant="contained" fullWidth>
+                  Pay now
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <Modal
@@ -55,17 +138,9 @@ const Checkout = () => {
         onClose={handleClose}
         aria-labelledby="parent-modal-title"
         aria-describedby="parent-modal-description"
-        
       >
-        <Box sx={ style }>
-          <div className="flex justify-between items-center">
-            <h1 className="text-xl font-bold">Contact Details</h1>
-            <IconButton onClick={handleClose}>
-              <Close/>
-            </IconButton>
-          </div>
-          <AddressForm/>
-          
+        <Box sx={style}>
+          <AddressForm onClose={handleClose} />
         </Box>
       </Modal>
     </>
--- src/customer/pages/Checkout/components/AddressForm/AddressForm.tsx
+++ src/customer/pages/Checkout/components/AddressForm/AddressForm.tsx
@@ -1,11 +1,142 @@
-import React from 'react'
+import { Close } from "@mui/icons-material";
+import { Box, Button, IconButton, TextField } from "@mui/material";
+import React from "react";
+import { useFormik } from 'formik';
+import * as Yup from 'yup'
 
-const AddressForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+
+
+interface Props {
+  onClose: () => void;
 }
 
-export default AddressForm
+const addressFormSchema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  mobile: Yup.string().required('Mobile is required'),
+  pinCode: Yup.string().required('Pin Code is required').matches(/^[0-9]{6}$/, 'Pin Code must be 6 digits'),
+  address: Yup.string().required('Address is required'),
+  city: Yup.string().required('City is required'),
+  state: Yup.string().required('State is required'),
+  locality: Yup.string().required('Locality is required')
+})
+
+const AddressForm = ({ onClose }: Props) => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      mobile: '',
+      pinCode: '',
+      address: '',
+      city: '',
+      state: '',
+      locality: ''
+    },
+    validationSchema: addressFormSchema,
+    onSubmit: (values) =>{
+      console.log(values)
+    }
+  });
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    <Box>
+      <div className="flex justify-between items-center pb-5">
+        <h1 className="text-xl font-bold">Contact Details</h1>
+        <IconButton onClick={handleClose}>
+          <Close />
+        </IconButton>
+      </div>
+      <form onSubmit={formik.handleSubmit}>
+        <div className=" grid grid-cols-12 gap-5" >
+          <div className="col-span-12">
+            <TextField
+              fullWidth
+              name="name"
+              label="Name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              error= {formik.touched.name && Boolean(formik.errors.name)}
+              helperText={formik.touched.name && formik.errors.name}
+            />
+          </div>
+          <div className="col-span-6">
+            <TextField
+              fullWidth
+              name="mobile"
+              label="Mobile"
+              value={formik.values.mobile}
+              onChange={formik.handleChange}
+              error= {formik.touched.mobile && Boolean(formik.errors.mobile)}
+              helperText={formik.touched.mobile && formik.errors.mobile}
+            />
+          </div>
+          <div className="col-span-6">
+            <TextField
+              fullWidth
+              name="pinCode"
+              label="PinCode"
+              value={formik.values.pinCode}
+              onChange={formik.handleChange}
+              error= {formik.touched.pinCode && Boolean(formik.errors.pinCode)}
+              helperText={formik.touched.pinCode && formik.errors.pinCode}
+            />
+          </div>
+          <div className="col-span-12">
+            <TextField
+              fullWidth
+              name="address"
+              label="Address"
+              value={formik.values.address}
+              onChange={formik.handleChange}
+              error= {formik.touched.address && Boolean(formik.errors.address)}
+              helperText={formik.touched.address && formik.errors.address}
+            />
+          </div>
+          <div className="col-span-12">
+            <TextField
+              fullWidth
+              name="city"
+              label="City"
+              value={formik.values.city}
+              onChange={formik.handleChange}
+              error= {formik.touched.city && Boolean(formik.errors.city)}
+              helperText={formik.touched.city && formik.errors.city}
+            />
+          </div>
+          <div className="col-span-6">
+            <TextField
+              fullWidth
+              name="state"
+              label="State"
+              value={formik.values.state}
+              onChange={formik.handleChange}
+              error= {formik.touched.state && Boolean(formik.errors.state)}
+              helperText={formik.touched.state && formik.errors.state}
+            />
+          </div>
+          <div className="col-span-6">
+            <TextField
+              fullWidth
+              name="locality"
+              label="Locality"
+              value={formik.values.locality}
+              onChange={formik.handleChange}
+              error= {formik.touched.locality && Boolean(formik.errors.locality)}
+              helperText={formik.touched.locality && formik.errors.locality}
+            />
+          </div>
+          
+        </div>
+        <div className="mt-8">
+        <Button type="submit" fullWidth variant="contained"  size="large">Add New Address</Button>
+        </div>
+      </form>
+    </Box>
+  );
+};
+
+export default AddressForm;
