f20d3b6dfe99463baba83a30b6ad74e4b349070c
nrin31266 <162881731+nrin31266@users.noreply.github.com>
2025-02-26 12:20:07 +0700

    admin menu

--- src/admin/pages/AdminDashboard/AdminDashboard.tsx
+++ src/admin/pages/AdminDashboard/AdminDashboard.tsx
@@ -2,15 +2,77 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import AdminDrawerList from "../../components/AdminDrawerList/AdminDrawerList";
 import { MenuItem } from "../../../components/DrawerList/DrawerList";
+import {
+  AccountBox,
+  Add,
+  Category,
+  Dashboard,
+  ElectricBolt,
+  Home,
+  IntegrationInstructions,
+  LocalOffer,
+  Logout,
+} from "@mui/icons-material";
 
-const menu : MenuItem[] = [
+const menu: MenuItem[] = [
+  {
+    name: "Dashboard",
+    path: "/admin",
+    icon: <Dashboard className="text-[var(--primary-color)]" />,
+    activeIcon: <Dashboard className="text-white" />,
+  },
+  {
+    name: "Coupon",
+    path: "/admin/coupon",
+    icon: <IntegrationInstructions className="text-[var(--primary-color)]" />,
+    activeIcon: <IntegrationInstructions className="text-white" />,
+  },
+  {
+    name: "Add New Coupon",
+    path: "/admin/add-new-coupon",
+    icon: <Add className="text-[var(--primary-color)]" />,
+    activeIcon: <Add className="text-white" />,
+  },
+  {
+    name: "Home Page",
+    path: "/admin/home-grid",
+    icon: <Home className="text-[var(--primary-color)]" />,
+    activeIcon: <Home className="text-white" />,
+  },
+  {
+    name: "Electronics Category",
+    path: "/admin/electronics-category",
+    icon: <ElectricBolt className="text-[var(--primary-color)]" />,
+    activeIcon: <ElectricBolt className="text-white" />,
+  },
+  {
+    name: "Shop By Category",
+    path: "/admin/shop-by-category",
+    icon: <Category className="text-[var(--primary-color)]" />,
+    activeIcon: <Category className="text-white" />,
+  },
+  {
+    name: "Deals",
+    path: "/admin/deals",
+    icon: <LocalOffer className="text-[var(--primary-color)]" />,
+    activeIcon: <LocalOffer className="text-white" />,
+  },
+];
 
-]
-
-const menu2 : MenuItem[] = [
-
-]
-
+const menu2: MenuItem[] = [
+  {
+    name: "Account",
+    path: "/admin/account",
+    icon: <AccountBox className="text-[var(--primary-color)]" />,
+    activeIcon: <AccountBox className="text-white" />,
+  },
+  {
+    name: "Logout",
+    path: "/",
+    icon: <Logout className="text-[var(--primary-color)]" />,
+    activeIcon: <Logout className="text-white" />,
+  },
+];
 
 const AdminDashboard = () => {
   const handleToggleDrawer = () => {};
