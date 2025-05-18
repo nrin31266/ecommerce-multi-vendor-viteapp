a3c056f6d101fa432af7f449c0136661bf55a884
nrin31266 <162881731+nrin31266@users.noreply.github.com>
2025-05-18 19:25:24 +0700

    fix

--- src/customer/pages/BecomeSeller/components/SellerLoginForm/SellerLoginForm.tsx
+++ src/customer/pages/BecomeSeller/components/SellerLoginForm/SellerLoginForm.tsx
@@ -65,7 +65,7 @@ const SellerLoginForm = () => {
                 <TextField
                   fullWidth
                   required
-                  type="number"
+                  type="text"
                   name="otp"
                   label="Otp"
                   value={formik.values.otp}
